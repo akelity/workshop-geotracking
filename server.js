020029ddd5b772f58042e3c89b03e512936a5394
Michel Murabito <michel.murabito@mia-platform.eu>
2022-10-28 07:55:57 +0200

    init step 2

--- server.js
+++ server.js
@@ -4,35 +4,9 @@ const app = express();
 
 // Init http and socket.io
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
 
 // http port to listen
 const port = 3000;
-
-// Store Position
-let sockets = {}
-
-// Socket.io
-io.on('connection', function(socket) {
-    console.log('Socket Connected ', socket.id);
-    sockets[socket.id] = socket;
-    
-    socket.on('positionToServer', function (data) {
-        console.log('positionToServer ', data);
-        Object.keys(sockets).forEach(key => {
-            sockets[key].emit('positionToClient', data);
-        });
-    });
-
-    //Whenever someone disconnects this piece of code executed
-    socket.on('disconnect', function () {
-        console.log('Socket disconnected');
-        Object.keys(sockets).forEach(key => {
-            sockets[key].emit('deleteToClient', socket.id);
-        });
-        delete sockets[socket.id]
-    });
-});
 
 // Create API  
 app.get('/api', (req, res) => {
